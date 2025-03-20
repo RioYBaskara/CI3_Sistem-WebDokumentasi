5c0bbecc220ba751aa6f3669f52eb53774121df8
Rio Yudha Baskara <156874101+RioYBaskara@users.noreply.github.com>
2025-03-20 23:41:50 +0700

    fix hover effect

--- application/views/admin/dokumentasi/header.php
+++ application/views/admin/dokumentasi/header.php
@@ -23,6 +23,11 @@
     <link href="<?= base_url(); ?>assets/tabler/dist/css/tabler-vendors.min.css?1692870487" rel="stylesheet" />
     <link href="<?= base_url(); ?>assets/tabler/dist/css/demo.min.css?1692870487" rel="stylesheet" />
     <link href="<?= base_url(); ?>assets/css/fontawesome/css/font-awesome.min.css" />
+    <!-- Bootstrap 5 CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Tabler CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/@tabler/core@latest/dist/css/tabler.min.css" rel="stylesheet">
     <style>
         @import url('https://rsms.me/inter/inter.css');
 
